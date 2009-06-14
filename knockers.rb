d5b9dc4769bd68f553343031c83463421afe4df0
Christian von Kleist <cvonkleist@gmail.com>
2009-06-14 11:26:58 -0400

    cleanup for prettiness

--- knockers.rb
+++ knockers.rb
@@ -4,38 +4,35 @@
 SHELL = '/bin/bash'
 
 # knocking command
-NC_TEMPLATE = {
+NC_KNOCK = {
   :client => 'nc -z #{server} #{port}',
   :server => 'nc -z -l -p#{port}'
 }
 # shell-exec'ing command
-NC_LAST = {
+NC_SHELL = {
   :client => 'nc #{server} #{port} -c' + SHELL,
   :server => 'nc -l -p#{port}'
 }
 
+# the server will bind (and client will connect) to ports in this range
 low_port, high_port = 1024, 65535
 
-if ARGV.length < 2
-  puts "usage: #{$0} key server"
-  exit 1
-end
+exit puts("usage: #{$0} key [server]") || 1 if ARGV.empty?
 
-key, server = ARGV.shift, ARGV.shift
+key, server = ARGV
 
-srand key.split('').inject(0) { |sum, n| sum + n[0] }
-ports = (0...key.length).collect do |i|
-  (rand(65536) + key[i]) % (high_port - low_port + 1) + low_port
-end
+# the ports are created randomly with the key as a seed for the RNG. this
+# should give big changes in ports with small changes in key.
+srand key.unpack('c*').inject { |sum, n| sum + n }
+ports = key.unpack('c*').collect { |i| (rand(65536) + i) % (high_port - low_port + 1) + low_port }
 
+# generate commands for client and server, but skip client command if server
+# isn't specified
 [:client, :server].each do |machine|
+  next if machine == :client && !server
   command = ports.collect do |port|
-    if port == ports.last
-      eval('"' + NC_LAST[machine] + '"')
-    else
-      eval('"' + NC_TEMPLATE[machine] + '"')
-    end
+    template = (port == ports.last ? NC_SHELL : NC_KNOCK)[machine]
+    eval('"' + template + '"')
   end.join(' && ')
   puts command
 end
-
